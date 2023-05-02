105565ae5c9e9b0a7f83c1fc3cd455e4adbc9d53
Ostap Holub <holub.ostap@gmail.com>
2023-05-02 17:17:26 +0300

    Replace deprecated API's

--- nylas_demo_app_v3/lib/feature/auth/domain/google_auth_cubit.dart
+++ nylas_demo_app_v3/lib/feature/auth/domain/google_auth_cubit.dart
@@ -9,7 +9,7 @@ class GoogleAuthCubit extends Cubit<bool> {
     final canLaunchUri = await canLaunchUrl(googleSignInUri);
     if (canLaunchUri) {
       emit(true);
-      await launch(googleSignInUri.toString());
+      await launchUrl(googleSignInUri);
     } else {
       emit(false);
     }
