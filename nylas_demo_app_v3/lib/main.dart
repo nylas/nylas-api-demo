0d2a32ae028a3b3530bd56fe0187dde84058217e
Ostap Holub <holub.ostap@gmail.com>
2023-05-02 16:24:18 +0300

    Add const keyword to prevent app to re create Center widget and children every time

--- nylas_demo_app_v3/lib/main.dart
+++ nylas_demo_app_v3/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-        body: Center(
+        body: const Center(
           child: AuthScreen()
         ),
       ),
