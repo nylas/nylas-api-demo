d2deffed0980cacca21935c2952588d1af9270f2
Ostap Holub <holub.ostap@gmail.com>
2023-05-02 16:23:26 +0300

    Setup application bar for an app

--- nylas_demo_app_v3/lib/feature/auth/ui/auth_screen.dart
+++ nylas_demo_app_v3/lib/feature/auth/ui/auth_screen.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:nylas_demo_app_v3/resources/strings.dart';
 
 class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(AppStrings.welcome),
-      ),
-      body: const Center(
-        child: Text(AppStrings.welcome),
-      ),
-    );
+    return const Text('Authentication screen');
   }
 }
