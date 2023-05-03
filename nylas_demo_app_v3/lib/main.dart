51dedc19b26588901606e6a4ea2847262f1248ca
Ostap Holub <holub.ostap@gmail.com>
2023-05-03 15:10:05 +0300

    Make the app start from config screen always

--- nylas_demo_app_v3/lib/main.dart
+++ nylas_demo_app_v3/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nylas_demo_app_v3/feature/auth/domain/google_auth_cubit.dart';
 import 'package:nylas_demo_app_v3/feature/auth/ui/google_auth_button.dart';
+import 'package:nylas_demo_app_v3/feature/config/ui/config_screen.dart';
 import 'package:nylas_demo_app_v3/resources/colors.dart';
 
 void main() {
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
           )
         ),
         body: const Center(
-          child: GoogleAuthButton()
+          child: ConfigScreen()
         ),
       ),
     );
